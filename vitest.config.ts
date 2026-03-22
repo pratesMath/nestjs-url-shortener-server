e6f5de923887e1e2c9ef4dba1b4b0e8accb83c04
pratesMath <developer.prates.matheus@gmail.com>
2026-03-22 03:22:05 -0300

    chore: new dependencies were added + Vitest configuration for unit tests.

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		include: ['**/*.spec.ts'],
+		exclude: ['data', 'node_modules', 'dist'],
+		globals: true,
+		root: './',
+	},
+	plugins: [
+		tsConfigPaths(),
+		swc.vite({
+			module: { type: 'es6' },
+		}),
+	],
+});
