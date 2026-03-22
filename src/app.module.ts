08a766ceed4036b46dcd927ece27562fc732bad3
pratesMath <developer.prates.matheus@gmail.com>
2026-03-22 16:57:08 -0300

    feat: EnvModule created and added to AppModule using nestjs cli. Now we're gonna use EnvService to handle w/ 'env' variables that includes zod lib for e2e type-safety ensuring a better development experience and avoiding errors during project execution by bad env vars configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { JWTAuthModule } from '@config/auth/jwt-auth.module';
+import { envSchema } from '@config/env/env';
+import { EnvModule } from '@config/env/env.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [JWTAuthModule],
+	imports: [
+		ConfigModule.forRoot({
+			validate: env => envSchema.parse(env),
+			isGlobal: true,
+		}),
+		JWTAuthModule,
+		EnvModule,
+	],
 })
 export class AppModule {}
---
+++ src/config/env/env.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod/v4';
+
+export const envSchema = z.object({
+	NODE_ENV: z.enum(['development', 'homolog', 'production']).default('production'),
+	PORT: z.coerce.number().optional().default(7070),
+	JWT_PRIVATE_KEY: z.string(),
+	JWT_PUBLIC_KEY: z.string(),
+	DATABASE_URL: z.url(),
+	PASSWORD_SALT: z.string().optional().default('8'),
+	PASSWORD_PEPPER: z.string(),
+	REDIS_DB: z.coerce.number().optional().default(0),
+	REDIS_HOST: z.string().optional().default('127.0.0.1'),
+	REDIS_PORT: z.coerce.number().optional().default(6379),
+});
+
+export type Env = z.infer<typeof envSchema>;
